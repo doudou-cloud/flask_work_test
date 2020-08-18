5decb0230a8e54eb2ce3c5e2d6ac629e6041f13a
doudou-cloud <307513146@qq.com>
2020-08-18 17:57:47 +0800

    封装嘻嘻哈哈函数

--- until/response_info.py
+++ until/response_info.py
@@ -11,3 +11,6 @@ def return_success_response(code=None,msg=None,data=None):
 
 def return_error_response(code=None,msg=None,data=None)
     return jsonify({})
+
+def return_xixi_response():
+    return jsonify({'嘻嘻哈哈':'阿兜'})
